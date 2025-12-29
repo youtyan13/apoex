114979d6baa3a3fdb0a26077e5a1fb72ee8db40f
YUsya <103307221+youtyan13@users.noreply.github.com>
2025-12-29 17:46:47 +0900

    ver.0.0.6-alpha file upload

--- src/main/java/com/youtyan/apoex/compat/mekanism/ApoExMekanismCompatModule.java
+++ src/main/java/com/youtyan/apoex/compat/mekanism/ApoExMekanismCompatModule.java
@@ -1,35 +1,32 @@
 package com.youtyan.apoex.compat.mekanism;
 
-import com.youtyan.apoex.compat.ICreativeTabListener;
-import com.youtyan.apoex.event.mekanism.MekSuitGemDamageHandler;
 import com.youtyan.apoex.item.ApoExItems;
 import com.youtyan.apoex.item.mekanism.MekSuitsGemItem;
 import moffy.addonapi.AddonModule;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.registries.RegistryObject;
 
-public class ApoExMekanismCompatModule extends AddonModule implements ICreativeTabListener {
+public class ApoExMekanismCompatModule extends AddonModule {
 
     public static RegistryObject<Item> MEK_SUITS_GEM;
 
     public ApoExMekanismCompatModule() {
-        MEK_SUITS_GEM = ApoExItems.ITEMS.register("mek_suits_gem", () -> new MekSuitsGemItem(new Item.Properties().rarity(Rarity.EPIC)));
-    }
-
-    @Override
-    public void buildContents(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(MEK_SUITS_GEM.get());
-        }
+        MEK_SUITS_GEM = ApoExItems.ITEMS.register("mek_suits_gem", () -> new MekSuitsGemItem(new Item.Properties()));
     }
 
     @Override
     public void setup(FMLCommonSetupEvent event) {
-        MinecraftForge.EVENT_BUS.register(MekSuitGemDamageHandler.class);
+        registerEvents();
+    }
+
+    private void registerEvents() {
+        try {
+            Class<?> handlerClass = Class.forName("com.youtyan.apoex.event.mekanism.MekSuitGemDamageHandler");
+            MinecraftForge.EVENT_BUS.register(handlerClass);
+        } catch (Throwable e) {
+            System.err.println("Failed to register MekSuitGemDamageHandler: " + e.getMessage());
+        }
     }
 }
--- src/main/java/com/youtyan/apoex/item/mekanism/MekSuitsGemItem.java
+++ src/main/java/com/youtyan/apoex/item/mekanism/MekSuitsGemItem.java
@@ -1,9 +1,6 @@
 package com.youtyan.apoex.item.mekanism;
 
 import com.youtyan.apoex.item.IExGem;
-import mekanism.common.content.gear.IModuleContainerItem;
-import mekanism.common.config.MekanismConfig;
-import mekanism.api.NBTConstants;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -16,15 +13,33 @@ public class MekSuitsGemItem extends Item implements IExGem {
 
     @Override
     public boolean canApplyTo(ItemStack targetStack) {
-        return !(targetStack.getItem() instanceof IModuleContainerItem);
+        try {
+            Class<?> moduleContainerClass = Class.forName("mekanism.common.content.gear.IModuleContainerItem");
+            return !moduleContainerClass.isInstance(targetStack.getItem());
+        } catch (ClassNotFoundException e) {
+            return true;
+        }
     }
     
     @Override
     public void onApplied(ItemStack stack) {
-        CompoundTag mekData = new CompoundTag();
-        mekData.putString("energy", MekanismConfig.gear.mekaSuitBaseEnergyCapacity.get().toString());
-        mekData.put(NBTConstants.GAS_TANKS, new CompoundTag());
-        mekData.put(NBTConstants.FLUID_TANKS, new CompoundTag());
-        stack.getOrCreateTag().put("mekData", mekData);
+        try {
+            Class<?> configClass = Class.forName("mekanism.common.config.MekanismConfig");
+            Object gearConfig = configClass.getField("gear").get(null);
+            Object energyCapacityConfig = gearConfig.getClass().getField("mekaSuitBaseEnergyCapacity").get(gearConfig);
+            Object energyCapacity = energyCapacityConfig.getClass().getMethod("get").invoke(energyCapacityConfig);
+            
+            Class<?> nbtConstantsClass = Class.forName("mekanism.api.NBTConstants");
+            String gasTanksKey = (String) nbtConstantsClass.getField("GAS_TANKS").get(null);
+            String fluidTanksKey = (String) nbtConstantsClass.getField("FLUID_TANKS").get(null);
+
+            CompoundTag mekData = new CompoundTag();
+            mekData.putString("energy", energyCapacity.toString());
+            mekData.put(gasTanksKey, new CompoundTag());
+            mekData.put(fluidTanksKey, new CompoundTag());
+            stack.getOrCreateTag().put("mekData", mekData);
+        } catch (Exception e) {
+            System.err.println("Failed to apply Mekanism data to item: " + e.getMessage());
+        }
     }
 }
